49c4dd76ef4f6fe7f592e67d3ae096750c47b8ea
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2020-06-06 15:02:35 +0900

    交差エントロピー誤差と平均交差エントロピー誤差

---
+++ machine_learning/classification/cross_entropy_error.py
@@ -0,0 +1,15 @@
+import numpy as np
+from logistic_regression import logistic_regression
+
+#交差エントロピー誤差
+def cross_entropy_error(w,x,t):
+    y=logistic_regression(w,x)
+    cee=0
+    for n in range(len(y)):
+        cee += -(t[n]*np.log(y[n]) + (1-t[n])*np.log(1-y[n]))
+    return cee
+
+
+#平均交差エントロピー誤差
+def ave_cross_entropy_error(w,x,t):
+    return cross_entropy_error(w,x,t)/len(y)
