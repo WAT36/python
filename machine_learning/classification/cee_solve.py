b1a571b8293e31a08a3ebc9ea27f9b590dea61ad
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2020-06-09 21:59:20 +0900

    scipy.optimize.minimize 使った平均交差エントロピー誤差の解導出　この関数覚えんと

---
+++ machine_learning/classification/cee_solve.py
@@ -0,0 +1,18 @@
+from cross_entropy_error import ave_cross_entropy_error
+from scipy.optimize import minimize
+from d_cee import d_cee
+import numpy as np
+
+def cee_solve(w_init,x,t):
+    ans = minimize(ave_cross_entropy_error,w_init,args=(x,t),jac=d_cee,method="CG")
+    return ans.x
+
+w_init=[1,1]
+
+#入力値
+x = np.load('x.npy')
+#実測値
+t = np.load('t.npy')
+
+w_ans=cee_solve(w_init,x,t)
+print(w_ans)
