71b8987846f5b0ee827cad95ccf5677fd0d27a98
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2020-06-17 20:04:54 +0900

    ２次元入力　ロジスティック回帰モデル　変更

--- machine_learning/classification/logistic_regression_2d.py
+++ machine_learning/classification/logistic_regression_2d.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sigmoid import sigmoid
 
+#ロジスティック回帰モデル(２次元入力)
 def logistic_regression_2d(w,x):
-    x = w[0]*x[0] + w[1]*x[1] + w[2]
+    x = w[0]*x[:,0] + w[1]*x[:,1] + w[2]
     return sigmoid(x)
