60d663dc0d1bca69a977713c910730108586d016
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2020-06-21 02:16:16 +0900

    meshgrid用のロジスティック回帰モデル計算用

--- machine_learning/classification/logistic_regression_2d.py
+++ machine_learning/classification/logistic_regression_2d.py
@@ -1,7 +1,13 @@
-import numpy as np
 from sigmoid import sigmoid
+import numpy as np
 
 #ロジスティック回帰モデル(２次元入力)
 def logistic_regression_2d(w,x):
     x = w[0]*x[:,0] + w[1]*x[:,1] + w[2]
     return sigmoid(x)
+
+#ロジスティック回帰モデル(２次元入力・プロット用)
+def logistic2d_for_plot(w,x0,x1):
+    x = w[0]*x0 + w[1]*x1 + w[2]
+    return sigmoid(x)
+
