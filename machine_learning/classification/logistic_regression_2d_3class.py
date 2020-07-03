b396cf01a972301445ff011bd26f1dd79a9c4b43
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2020-07-03 23:16:51 +0900

    ２次元入力３クラス分類　の　平均交差エントロピー誤差　の　偏微分

--- machine_learning/classification/logistic_regression_2d_3class.py
+++ machine_learning/classification/logistic_regression_2d_3class.py
@@ -31,6 +31,21 @@ def cross_entropy_error_for_2d_3class(w,x,t):
     return cee
 
 
+#平均交差エントロピー誤差の偏微分（２次元入力３クラス分類用）
+def d_cee_for_2d_3class(w,x,t):
+    #w:3*3行列
+    #x:n*2行列（xの転置）
+    #t:n*3行列（t[i]がクラスkにb分類された時t[i.k]=1,それ以外は0）
+    y=logistic_regression_2d_3class(w,x)
+    #d_cee:3*3 (クラスの数k*(xの次元+1)) 行列
+    d_cee=np.zeros((3,3))
+    N=x.shape[0]
+    for n in range(N):
+        for k in range(3):
+            d_cee[k,:] = d_cee[k,:] + (y[n,k]-t[n,k])*np.array(x[n,:]+[1])
+    d_cee = d_cee / N
+    return d_cee
+
 
 
 
