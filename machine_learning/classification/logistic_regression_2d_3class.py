de6c6982e4796c0e40924361d5802a37cc6c0c15
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2020-07-11 12:31:55 +0900

    ２次元入力３クラス分類　肩直した

--- machine_learning/classification/logistic_regression_2d_3class.py
+++ machine_learning/classification/logistic_regression_2d_3class.py
@@ -4,27 +4,28 @@ import numpy as np
 
 #ロジスティック回帰モデル(２次元入力３クラス分類)
 def logistic_regression_2d_3class(w,x):
-    #w:3*3行列
+    #w:1*9行列 -> 3*3行列
+    w=w.reshape((3,3))
     #x:n*2行列 (xの転置)
     n=len(x)
     #a:n*3行列
     a=np.zeros((n,3))
-    w=w.reshape((3,3))
     for k in range(3):
         a[:,k] = np.exp( w[k,0]*x[:,0] + w[k,1]*x[:,1] +w[k,2] )
-    #u:n*1行列、aの１行の要素の合計
+    #u:aの１行の要素の合計、n*1行列にする
     u = np.sum(a,axis=1)
     u = u.reshape(n,1)
+    #y:n*3行列
     y = a/u
     return y
 
 
 #平均交差エントロピー誤差（２次元入力３クラス分類用）
 def cross_entropy_error_for_2d_3class(w,x,t):
-    #w:3*3行列
+    #w:1*9行列 -> 3*3行列
+    w=w.reshape((3,3))
     #x:n*2行列（xの転置）
     #t:n*3行列（t[i]がクラスkにb分類された時t[i.k]=1,それ以外は0）
-    w=w.reshape((3,3))
     y=logistic_regression_2d_3class(w,x)
     N=y.shape[0]
     #cee:平均交差エントロピー誤差
@@ -38,10 +39,10 @@ def cross_entropy_error_for_2d_3class(w,x,t):
 
 #平均交差エントロピー誤差の偏微分（２次元入力３クラス分類用）
 def d_cee_for_2d_3class(w,x,t):
-    #w:3*3行列
+    #w:1*9行列 -> 3*3行列
+    w=w.reshape((3,3))
     #x:n*2行列（xの転置）
     #t:n*3行列（t[i]がクラスkにb分類された時t[i.k]=1,それ以外は0）
-    w=w.reshape((3,3))
     y=logistic_regression_2d_3class(w,x)
     #d_cee:3*3 (クラスの数k*(xの次元+1)) 行列
     d_cee=np.zeros((3,3))
@@ -50,6 +51,7 @@ def d_cee_for_2d_3class(w,x,t):
         for k in range(3):
             d_cee[k,:] = d_cee[k,:] + (y[n,k]-t[n,k])*np.r_[x[n,:],1]
     d_cee = d_cee / N
+    #3*3->1*1行列(minimizeの仕様上)で返す
     return d_cee.reshape(-1)
 
 #勾配法
