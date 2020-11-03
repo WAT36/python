c086922d3f88c899cb3185567c6f344af36b20f6
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2020-11-03 11:16:01 +0900

    バケット法のコード　例追記

--- procon/bucket.py
+++ procon/bucket.py
@@ -6,7 +6,7 @@ class Bucket:
         #元のリスト
         self.a=a
         #平方分割用バケット用意
-        b=int(math.sqrt(b)//1)
+        b=int(math.sqrt(len(a))//1)
         #１バケットあたりのデータの個数
         self.b=b
         bucket=[]
@@ -24,7 +24,7 @@ class Bucket:
         self.bucket=bucket
     
     #a[s]~a[t]の最小値を求める
-    def query(s,t):
+    def query(self,s,t):
         ans=INF
         x=s
 
@@ -38,14 +38,14 @@ class Bucket:
             return ans
         #(そうでないとき)バケット計算
         else:
-            while (x+b<=t):
-                ans=min(ans,self.bucket[x//b])
-                x+=b
+            while (x+self.b<=t):
+                ans=min(ans,self.bucket[x//self.b])
+                x+=self.b
         
         #(~a[t])個々の要素の計算
-        if(t-x+1==b):
+        if(t-x+1==self.b):
             #(a[t]がバケット内の最後の要素であったとき)
-            ans=min(ans,self.bucket[x//b])
+            ans=min(ans,self.bucket[x//self.b])
             return ans
         else:
             #そうでないときは１個ずつ見る
@@ -55,9 +55,19 @@ class Bucket:
             return ans
 
     #a[i]をxに変更
-    def update(i,x):
+    def update(self,i,x):
         self.a[i]=x
         #a[i]があるバケットのインデックス
         j=i//self.b
         self.bucket[j]=min(self.bucket[j],x)
+
+a=[5,2,3,7,4,1,9,10]
+buc=Bucket(a)
+
+#a[1]~a[4]までの最小値
+print("a{0}~a{1}までの最小値:{2}".format(1,4,buc.query(1,4)))
+
+#a[0]を0に変える
+buc.update(0,0)
+print("バケット:{}".format(buc.a))
 
