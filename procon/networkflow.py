383c37ed61d902efc89bf70cf1653056aa3e77e4
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-02-19 09:11:07 +0900

    復活　ネットワークフロー のコードを動く様にした

--- procon/networkflow.py
+++ procon/networkflow.py
@@ -6,23 +6,34 @@ class edge:
             self.cap=cap
             self.rev=rev
 
-#入力(頂点の数)
+#頂点の数
 V=0
 
 #グラフ
-G=[[] for _ in range(V)]
+G=[]
 #DFSですでに使われたかのフラグ
-used=[False for _ in range(V)]
+used=[]
+
+#初期化
+def init(v):
+    global G
+    global used
+    G=[[] for _ in range(v)]
+    used=[False for _ in range(v)]
 
 #fromからtoへ向かう容量capの辺をグラフに追加する
 def add_edge(from_v,to_v,cap):
+    global G
     #from->toの容量capの辺,逆辺はto->fromの辺
     G[from_v].append(edge(to_v,cap,len(G[to_v])))
-    G[to_v].append(edge(from_v,0,len(G[from_v]))-1)
+    G[to_v].append(edge(from_v,0,len(G[from_v])-1))
 
 
 #増加パスをDFSで探す
 def dfs(v,t,f):
+    global G
+    global used
+    print(v,t,f,used)
     if(v==t):
         return f
     used[v]=True
@@ -32,19 +43,35 @@ def dfs(v,t,f):
             d = dfs(e.to,t,min(f,e.cap))
             if(d>0):
                 e.cap-=d
-                G[e.to][e.rev].cap+=0
+                G[e.to][e.rev].cap+=d
                 return d
     return 0
 
 #sからtへの最大流を求める
 def max_flow(s,t):
+    global used
     flow=0
     while True:
+        used=[False for _ in range(V)]
         f=dfs(s,t,INF)
+        print(flow,f)
         if(f==0):
             return flow
         flow+=f
 
+#初期化
+V=5
+init(V)
+
 #入力(辺)
+add_edge(0,1,10)
+add_edge(0,2,2)
+add_edge(1,2,6)
+add_edge(1,3,6)
+add_edge(3,2,3)
+add_edge(2,4,5)
+add_edge(3,4,8)
 
-
+#実行
+ans=max_flow(0,4)
+print(ans)
